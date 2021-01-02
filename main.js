168024ec9e3338ebde15a55485cdcb5a4e0c79fb
Toni Hamalainen <tomahama@student.jyu.fi>
2021-01-02 15:42:41 +0200

    added oop movie and movies

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 "use strict";
 
-console.log("hey!");
+class Movie {
+    constructor(name,releaseDate,genre,actors,length){
+        this.name=name;
+        this.releaseDate =releaseDate;
+        this.genre = genre;
+        this.actors = actors;
+        this.length = length;
+    }
+    getName(){
+        return this.name;
+    }
+}
+
+class MovieCollection{
+    movies = [];
+
+    constructor(movies){
+    this.movies = movies;
+    }
+
+    addMovie(movie){
+        this.movies.push(movie);
+    }
+
+}
+
+let movie1 = new Movie("Batman","13.2.1999","Action",["Christian Bale","Liam Neeson"],"2h");
+let allMovies = new MovieCollection();
+allMovies.addMovie(movie1);
+console.log(allMovies);
