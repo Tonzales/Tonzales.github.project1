9f47826f70a5778ced0d2ad21cb5d5542bd0340f
Toni Hamalainen <tomahama@student.jyu.fi>
2021-01-02 21:02:11 +0200

    added styles, list items etc.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import  Movie from "./Movie.js";
 import MovieCollection from "./MovieCollection.js";
 
+let container = document.getElementById("container");
+let list = document.getElementById("movieList");
 
 let movie1 = new Movie("Batman","13.2.1999","Action",["Christian Bale","Liam Neeson"],"2h");
 let movie2 = new Movie("The Dark Knight","13.2.2002","Action",["Christian Bale","Heath Ledger"],"2h");
@@ -9,9 +11,16 @@ let movieArray = [movie1,movie2,movie3];
 let allMovies = new MovieCollection();
 for(let i = 0; i < movieArray.length; i++){
     allMovies.addMovie(movieArray[i]);
+    let movieElement = document.createElement("li");
+    movieElement.setAttribute("class","listItem") 
+    let paragraph = document.createElement("p");
+    paragraph.textContent=movieArray[i].getName();
+    paragraph.setAttribute("style","color:white;");
+    list.appendChild(movieElement);
+    movieElement.appendChild(paragraph);
+
 }
 
-let container = document.getElementById("container");
-let header = document.createElement("h1");
-header.textContent="Reeler";
-container.appendChild(header);
+
+
+
